b13ea55af96c7a6c2e2d16390dc020781e9b0926
CarlosUni <100074907@alumnos.uc3m.es>
2019-03-09 21:57:37 +0100

    Preparacion proyecto

---
+++ pacman/launch_busters.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Lanzador de Pac-Man modo busters en el mapa oneHunt
+echo Ejecutando pacman-busters
+#python busters.py -n 1 -l oneHunt -p BasicAgentAA -g RandomGhost -k 4 -t 0.25
+
+python busters.py -p BasicAgentAA
+
+
